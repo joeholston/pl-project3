5aba9b3b39012724282b62924c47f45928798ddf
Joe Holston <holstonjx1@gcc.edu>
2019-04-11 14:53:40 -0400

    QueryStopWords implemented

--- Query.scala
+++ Query.scala
@@ -7,8 +7,16 @@ class Query(val words: Seq[String]) extends Weighted[String]{
     }
 }
 
-class QueryLength(override val words: Seq[String]) extends Query(words){
+class QueryStopWords(override val words: Seq[String]) extends Query(words){
+    val stopWords = List("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now")
     override def getWeights() = {
-        (for(word <- words) yield word.length.toDouble).toSeq
+        (for(word <- words) yield 
+        if(stopWords.contains(word)){
+            0.0
+        }
+        else{
+            1.0
+        }
+        ).toSeq
     }
 }
